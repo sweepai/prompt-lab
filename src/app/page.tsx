429e81aec5f568127e7000f12b24e56f5bbd5be5
Kevin Lu <kevinlu1248@gmail.com>
2023-11-20 18:26:59 -0800

    MVP regex colorizer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,85 @@
-import Image from 'next/image'
+import React from "react";
+import "./Highlighter.css"; // Import the CSS file for styling
+
+type RegexHighlighterProps = {
+  text: string;
+  regex: RegExp;
+};
+
+function randomHexColor(seed: number) {
+  return [
+    "darkgreen", "darkred", "darkblue", "brown"
+  ][seed % 5]
+}
+
+const RegexHighlighter: React.FC<RegexHighlighterProps> = ({ text, regex }) => {
+  const getHighlightedText = () => {
+    const result: JSX.Element[] = [];
+    let lastIndex = 0;
+
+    text.replace(regex, (match, ...args) => {
+      console.log(match, args)
+      const matchStart = args[args.length - 2];
+      const matchEnd = matchStart + match.length;
+
+      result.push(<span key={`unmatched-${lastIndex}`}>{text.substring(lastIndex, matchStart)}</span>);
+
+      let currentIndex = matchStart;
+
+      const colorChanges: [number, number, boolean][] = [];
+
+      var firstEnd = matchEnd + 1;
+
+      // @ts-ignore
+      regex.exec(match)!.indices.forEach(([start, end]: [number, number], index: number) => { 
+        console.log(start ,end)
+        colorChanges.push([start + matchStart, index, true]);
+        colorChanges.push([end + matchStart, index, false]);
+        if (start > 0) {
+          firstEnd = Math.min(firstEnd, start + matchStart);
+        }
+      })
+
+      colorChanges.sort((a, b) => a[0] - b[0]);
+      console.log(colorChanges)
+
+      var depth = 0;
+      for (let i = 0; i < colorChanges.length - 1; i++) {
+        const [index, colorIndex, isStart] = colorChanges[i];
+        depth += isStart ? 1 : -1;
+        result.push(
+          <span key={`match-${currentIndex}`} style={{ backgroundColor: randomHexColor(depth - 1) }}>
+            {text.substring(index, colorChanges[i + 1][0])}
+          </span>
+        );
+      }
+
+      lastIndex = matchEnd;
+      return match;
+    });
+
+    if (lastIndex < text.length) {
+      result.push(<span key={`remaining-${lastIndex}`}>{text.substring(lastIndex)}</span>);
+    }
+
+    return result;
+  };
+
+  return (
+    <div>
+      {getHighlightedText()}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="h-screen w-full flex">
+        <div className="w-1/3">Chat history goes here.</div>
+        <div className="w-2/3">
+          <RegexHighlighter regex={/text (to) be high(li(g)ht)ed/dgs} text="text to be highlighted" />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
