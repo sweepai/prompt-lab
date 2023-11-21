7c277d79b0a91a3d1bd489ee035a74605de51b37
Kevin Lu <kevinlu1248@gmail.com>
2023-11-20 18:58:11 -0800

    Works now

--- src/app/RegexVisualizer.tsx
+++ src/app/RegexVisualizer.tsx
@@ -9,13 +9,18 @@ const RegexVisualizer = ({text}: {text: string}) => {
     const ref = useRef<HTMLInputElement>(null)
     const onChange = () => {
         if (ref.current) {
-            setRegex(new RegExp(ref.current.value, "dgs"))
+            try {
+                setRegex(new RegExp(ref.current.value, "dgs"))
+            } catch (e: any) {
+                console.error(e)
+
+            }
         }
     }
     return (
         <div>
             <h1>Regex Visualizer</h1>
-            <input type="text" ref={ref} onChange={onChange} defaultValue={defaultRegex} />
+            <input type="text" ref={ref} onChange={onChange} defaultValue={defaultRegex} className="bg-gray-700 text-white border border-gray-600 rounded p-2"/>
             <br />
             <RegexHighlighter regex={regex} text={text}/>
         </div>
