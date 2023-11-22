4b69a5b31dfbbfeb7eacb3393d972e99410f8e66
Kevin Lu <kevinlu1248@gmail.com>
2023-11-21 17:41:25 -0800

    Regex fixes

--- src/app/RegexHighlighter.tsx
+++ src/app/RegexHighlighter.tsx
@@ -18,16 +18,23 @@ const RegexHighlighter: React.FC<RegexHighlighterProps> = ({ text, regex }) => {
       const matchStart = args[args.length - 2];
       const matchEnd = matchStart + match.length;
 
-      result.push(<span key={`unmatched-${lastIndex}`}>{text.substring(lastIndex, matchStart)}</span>);
+      result.push(<span className={`unmatched-${lastIndex}`}>{text.substring(lastIndex, matchStart)}</span>);
 
       let currentIndex = matchStart;
 
       const colorChanges: [number, boolean][] = [];
 
       var firstEnd = matchEnd + 1;
+      const exec_match = regex.exec(match)
+    //   console.log("regex.exec(match)", exec_match)
+
+      if (exec_match == null || exec_match.indices === undefined) {
+        // console.log(exec_match)
+        return match;
+      }
 
       // @ts-ignore
-      regex.exec(match)!.indices.forEach(([start, end]: [number, number], index: number) => { 
+      exec_match.indices.forEach(([start, end]: [number, number]) => {
         colorChanges.push([start + matchStart, true]);
         colorChanges.push([end + matchStart, false]);
         if (start > 0) {
@@ -42,7 +49,7 @@ const RegexHighlighter: React.FC<RegexHighlighterProps> = ({ text, regex }) => {
         const [index, isStart] = colorChanges[i];
         depth += isStart ? 1 : -1;
         result.push(
-          <span key={`match-${currentIndex}`} style={{ backgroundColor: randomHexColor(depth - 1) }}>
+          <span className={`match-${currentIndex}`} style={{ backgroundColor: randomHexColor(depth - 1) }}>
             {text.substring(index, colorChanges[i + 1][0])}
           </span>
         );
@@ -53,13 +60,14 @@ const RegexHighlighter: React.FC<RegexHighlighterProps> = ({ text, regex }) => {
     });
 
     if (lastIndex < text.length) {
-      result.push(<span key={`remaining-${lastIndex}`}>{text.substring(lastIndex)}</span>);
+      result.push(<span className={`remaining-${lastIndex}`}>{text.substring(lastIndex)}</span>);
     }
 
     return result;
   };
 
   const highlightedText = getHighlightedText()
+  console.log("highlightedText", highlightedText)
 
   return (
     <div className="font-mono p-2 bg-gray-900 border border-gray-700 rounded p-2 font-mono w-full">
